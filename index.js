9e4d79f876f21aba737bdf6c48df541b35e23bf4
ocowchun <ocowchun@gmail.com>
2016-05-20 22:51:17 +0800

    add pull command

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 
 var program = require('commander');
 var dynamodb = require('./lib/dynamodb');
-
+var fs=require('fs');
 
 program
     .version('0.1.0')
@@ -25,8 +25,27 @@ program
     .option("-t, --table <table_name>", "Which table to get")
     .action(function(options) {
         var tableName = options.table || 'gaga_config';
-        dynamodb.pull(tableName).then(console.log).catch(console.log)
+        dynamodb.getItem(tableName).then(console.log).catch(console.log)
     });
+
+program
+    .command('pull')
+    .description('pull config content')
+    .option("-t, --table <table_name>", "Which table to get")
+    .action(function(options) {
+        var tableName = options.table || 'gaga_config';
+        dynamodb.getItem(tableName).then(function(data) {
+            var fileName = 'config.json';
+            var content=JSON.stringify(data, null, '\t');
+            return new Promise(function(resolve, reject) {
+                fs.writeFile(fileName, content, (err) => {
+                    if (err) reject(err);
+                    console.log(fileName);
+                });
+            });
+        }).catch(console.log)
+    });
+
 
 
 program.parse(process.argv);
