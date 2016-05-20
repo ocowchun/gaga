7df3a94b8e645f86bb14778e4683b0670f527ba4
ocowchun <ocowchun@gmail.com>
2016-05-20 22:35:40 +0800

    add show command

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 #gaga
 >manage configuration with dynamodb
 
+
+todo
+
+config have version 
+
+current
+created_at
+
+
+version1
+version2....
+
 MIT
--- index.js
+++ index.js
@@ -6,6 +6,8 @@
 
 
 var program = require('commander');
+var dynamodb = require('./lib/dynamodb');
+
 
 program
     .version('0.1.0')
@@ -16,5 +18,15 @@ program
     .action(function() {
         console.log('yo');
     });
+
+program
+    .command('show')
+    .description('show config content')
+    .option("-t, --table <table_name>", "Which table to get")
+    .action(function(options) {
+        var tableName = options.table || 'gaga_config';
+        dynamodb.pull(tableName).then(console.log).catch(console.log)
+    });
+
 
 program.parse(process.argv);
